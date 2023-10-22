3bee3da008d2b069f7b21ef94224537296d2a1a3
wakayinja frank <142891493+wakayinjaF@users.noreply.github.com>
2023-10-22 19:12:08 +0300

    added new files

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,23 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Los Angeles", "Chicago", "Boston"];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
